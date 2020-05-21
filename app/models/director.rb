0e015bf3a8f1d97a81d328a240de4e2245175241
coralie delpha <coralie.delpha@gmail.com>
2020-05-21 21:19:58 +0200

    pg search multi search

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,3 +1,6 @@
 class Director < ApplicationRecord
   has_many :movies
+
+  include PgSearch::Model
+  multisearchable against: [:first_name, :last_name]
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
+  include PgSearch::Model
+  multisearchable against: [:title, :syllabus]
 end
